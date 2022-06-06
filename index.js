64fe9b589d9ea1802ea99462079be3a7355f6430
kiwi-ui <yusnianggara@gmail.com>
2022-06-06 21:57:31 +0700

    .

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 
 // TODO: Fungsi utama
-// Menerima 2 parameter: list nama dan callback sort
+ // Menerima 2 parameter: list nama dan callback sort
 // Mengembalikan array of string dengan format:
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (array,fn) => {
+  let sortedArray = fn(array)
+  
+  let hasil = []
+  for (let index = 0; index < array.length; index++) {
+    let hasil1 = "";
+    hasil1 = hasil.push(`${index+1}.${array[index]}`);
+  } 
+  return hasil;
+  }
+
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (param1) => {
+  let hasilFn = param1.sort()
+  return hasilFn
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (param1) => {
+  let hasilFn = param1.sort().reverse()
+  return hasilFn
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
